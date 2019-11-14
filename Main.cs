42207b10ebd52efbdebc22e5af45b2bb003e9c27
Robbie Page <robbie.page@e-gineering.com>
2019-11-13 20:34:23 -0500

    initial commit

---
+++ Main.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Wox.Plugin.WDWNTunes
+{
+    public class Main : IPlugin
+    {
+        public void Init(PluginInitContext context) { }
+        public List<Result> Query(Query query)
+        {
+            var currentSongInfo = GetCurrentSongInfo();
+
+            return new List<Result>
+            {
+                new Result
+                {
+                    IcoPath = $"Images\\ntunes.png",
+                    SubTitle = $"{currentSongInfo.Artist}",
+                    Title = $"{currentSongInfo.Title}",
+                    Action = _ => { return true; }
+                }
+            };
+        }
+
+        private NTunesCurrentTrack GetCurrentSongInfo()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = Task.Run(async () => await httpClient.GetStringAsync("http://fastpass.wdwnt.com/live365")).Result;
+                var jsonObject = JObject.Parse(response);
+
+                return new NTunesCurrentTrack
+                {
+                    Artist = (string)jsonObject["current-track"]["artist"],
+                    Title = (string)jsonObject["current-track"]["title"],
+                };
+            }
+        }
+    }
+
+    public class NTunesCurrentTrack
+    {
+        public string Artist { get; set; }
+        public string Title { get; set; }
+    }
+}
